e710e21ed28552194c58fb6885d23948ac5a8e70
ginacollecchia <ginacollecchia@gmail.com>
2020-08-19 16:47:15 -0600

    Fixed broken link to tone.js. Now draws circles smallest-to-biggest.

--- scripts/main.js
+++ scripts/main.js
@@ -5,22 +5,23 @@ function setup() {
   background(bgCol);
   textSize(22);
   
-}
-
-function draw() {
   // WORMHOLE BACKGROUND
   // draw concentric black and white circles
-  // ellipse(centerX, centerY, width, height)
-
   let diameter = windowWidth + 100;
   let thickness = 60;
   var i;
-  for (i = 0; i < ceil(windowWidth / thickness); i++) {
+  for (i = ceil(windowWidth / thickness) + 1; i >= 0; --i) {
     strokeWeight(thickness);
     stroke(255 + 255*Math.pow(-1,i)); // alternate color between black and white
+    noFill();
     // start with smallest ellipse
-    ellipse(windowWidth / 2, windowHeight / 2, diameter - i*thickness, diameter - i*thickness);    
+    circle(windowWidth / 2, windowHeight / 2, diameter - i*thickness); 
+    console.log(diameter - i*thickness);   
   }
+  
+}
+
+function draw() {
   
   
 }
