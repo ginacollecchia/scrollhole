faadbc44a4ca1e9bce778869b09284a6828f554d
ericheep <ericheep@gmail.com>
2020-08-19 20:47:17 -0600

    :fire: test mousewheel

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,19 @@ function setup() {
   bgCol = color(207, 236, 207); // minty
   background(bgCol);
   textSize(22);
-  
+
+}
+
+function mouseWheel(event) {
+  print(event.delta);
+  //move the square according to the vertical scroll amount
+  pos += event.delta;
+  //uncomment to block page scrolling
+  //return false;
 }
 
 function mouseMoved() {
-  
+
   // WORMHOLE BACKGROUND
   let diameter = windowWidth + 100;
   let thickness = 60;
@@ -20,7 +28,7 @@ function mouseMoved() {
     stroke(255 + 255*Math.pow(-1,i)); // alternate color between black and white
     noFill();
     // start with smallest circle. center relative to mouse position and center of screen. outermost circle should be the same every time regardless of mouse position.
-    // circle(windowWidth / 2, windowHeight / 2, diameter - i*thickness); 
+    // circle(windowWidth / 2, windowHeight / 2, diameter - i*thickness);
     circle((mouseX * i/numCircles + windowWidth/2 * (numCircles - i)/numCircles), (mouseY * i/numCircles + windowHeight/2 * (numCircles - i)/numCircles), diameter - i*thickness);
     // console.log(diameter - i*thickness);
   }
@@ -34,7 +42,7 @@ function mouseMoved() {
     noFill();
     arc((mouseX * (numCircles - i)/numCircles + windowWidth/2 * i/numCircles), (i+2)*thickness + (mouseY * (numCircles - i)/numCircles + windowHeight/2 * i/numCircles), (i+3)*thickness, (i+3)*thickness, -PI, PI);
   }
-  
+
   return false;
 }
 
