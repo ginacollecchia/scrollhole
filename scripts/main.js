95aebaa0e50e8c66587b988dd60d91477ffab605
ericheep <ericheep@gmail.com>
2020-08-22 16:33:07 -0600

    :wrench: :fire: moving stuff around

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,7 @@ function setup() {
   bgCol = color(207, 236, 207) // minty
   background(bgCol)
   textSize(22)
-  
+
   buffer = new Tone.Buffer('./audio/riddim.mp3')
   grainPlayer = new Tone.GrainPlayer(buffer).toDestination()
 
@@ -32,9 +32,9 @@ function mouseWheel(event) {
     } else {
       grainPlayer.reverse = false
     }
-        
+
     // console.log('player loaded');
-  } 
+  }
 }
 
 function keyPressed(event) {
@@ -51,34 +51,34 @@ function keyPressed(event) {
   }
 }
 
-function mouseMoved() {
+// function mouseMoved() {
 
-  // WORMHOLE BACKGROUND
-  let diameter = windowWidth + 100
-  let thickness = 60
-  var i
-  let numCircles = ceil(windowWidth / thickness) + 1
-  // draw concentric black and white circles in background
-  for (i = numCircles; i >= -1; i--) {
-    strokeWeight(thickness)
-    stroke(255 + 255*Math.pow(-1,i)) // alternate color between black and white
-    noFill()
-    // start with smallest circle. center relative to mouse position and center of screen. outermost circle should be the same every time regardless of mouse position.
-    // circle(windowWidth / 2, windowHeight / 2, diameter - i*thickness)
-    circle((mouseX * i/numCircles + windowWidth/2 * (numCircles - i)/numCircles), (mouseY * i/numCircles + windowHeight/2 * (numCircles - i)/numCircles), diameter - i*thickness)
-    // console.log(diameter - i*thickness)
-  }
+//   // WORMHOLE BACKGROUND
+//   let diameter = windowWidth + 100
+//   let thickness = 60
+//   var i
+//   let numCircles = ceil(windowWidth / thickness) + 1
+//   // draw concentric black and white circles in background
+//   for (i = numCircles; i >= -1; i--) {
+//     strokeWeight(thickness)
+//     stroke(255 + 255*Math.pow(-1,i)) // alternate color between black and white
+//     noFill()
+//     // start with smallest circle. center relative to mouse position and center of screen. outermost circle should be the same every time regardless of mouse position.
+//     // circle(windowWidth / 2, windowHeight / 2, diameter - i*thickness)
+//     circle((mouseX * i/numCircles + windowWidth/2 * (numCircles - i)/numCircles), (mouseY * i/numCircles + windowHeight/2 * (numCircles - i)/numCircles), diameter - i*thickness)
+//     // console.log(diameter - i*thickness)
+//   }
 
-  // draw "path" of circles leading up to center circle
-  // let arcDiameter = windowHeight/2
-  let numArcs = floor(numCircles/2) + 1
-  for (i = 0; i < numCircles; i++) {
-    strokeWeight(thickness)
-    stroke(255 + 255*Math.pow(-1,i)) // alternate color between black and white
-    noFill()
-    arc((mouseX * (numCircles - i)/numCircles + windowWidth/2 * i/numCircles), (i+2)*thickness + (mouseY * (numCircles - i)/numCircles + windowHeight/2 * i/numCircles), (i+3)*thickness, (i+3)*thickness, -PI, PI)
-  }
+//   // draw "path" of circles leading up to center circle
+//   // let arcDiameter = windowHeight/2
+//   let numArcs = floor(numCircles/2) + 1
+//   for (i = 0; i < numCircles; i++) {
+//     strokeWeight(thickness)
+//     stroke(255 + 255*Math.pow(-1,i)) // alternate color between black and white
+//     noFill()
+//     arc((mouseX * (numCircles - i)/numCircles + windowWidth/2 * i/numCircles), (i+2)*thickness + (mouseY * (numCircles - i)/numCircles + windowHeight/2 * i/numCircles), (i+3)*thickness, (i+3)*thickness, -PI, PI)
+//   }
 
-  return false
-}
+//   return false
+// }
 
